580991aa8b953c0580d55e9434cb15bc28e3cac3
littleflute <13699175164@sina.cn>
2017-07-19 14:09:43 -0500

    Add files via upload

---
+++ Beatallica/Sgt. Hetfield's Motorbreath Pub Band/m.js
@@ -0,0 +1,15 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Sgt. Hetfield's Motorbreath Pub Band.mp3";		
+f[2]	= "03 Blackened the U.S.S.R.mp3";		
+f[3]	= "05 Helvester of Skelter.mp3";		
+f[4]	= "06 A Garage Dayz Nite.mp3";		
+f[5]	= "07 Anesthesia (I'm Only Sleeping).mp3";		
+f[6]	= "08 Leper Madonna.mp3";		
+f[7]	= "09 Ktulu (He's So Heavy).mp3";		
+f[8]	= "10 For Horsemen.mp3";		
+f[9]	= "11 Hey Dude.mp3";		
+f[10]	= "12 Sgt. Hetfield's (Reprise).mp3";		
+f[11]	= "13 ...And Justice for All My Loving.mp3";		
+f[12]	= "Revol-Ooh-Tion.mp3";		
+f[13]	= "Sandman.mp3";		
